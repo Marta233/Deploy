72fa65f479f84564046b8dd0e129f87ddeed04be
martaassefa <martaassefa23@gmail.com>
2024-02-09 14:12:43 +0300

    Ss

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import About from "./Componentes/About/About.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 function App() {
   return (
     <div >
